c897a52dc49a5affd12028b1c56ff8747ee7c16c
keonho-kim <keonhok0315@gmail.com>
2021-02-19 16:35:54 -0500

    update

--- util.py
+++ util.py
@@ -1,6 +1,14 @@
+def to_list(series):
+	"""
+	variable is a list stored as string in DataFrame
+	"""		
+	from ast import literal_eval
+	series = series.apply(literal_eval)
+	return series
+
 def to_1D(series):
-	import pandas as pd
 	"""
 	variable should be Pandas Series
 	"""
+	import pandas as pd 
 	return pd.Series([x for _list in series for x in _list])
