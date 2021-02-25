0bf667c1cf2b7dc1ee967e131928437a738aaa86
keonho-kim <keonhok0315@gmail.com>
2021-02-25 17:15:19 -0500

    update

--- util.py
+++ util.py
@@ -212,7 +212,7 @@ def clean_comments(comments, tickers, dollar_tickers, dollar_tickers_lower, stoc
 
 
 def convert_time(str_time):
-	import datatime
+	import datetime
 	
 	converted_time = datetime.datetime.fromtimestamp(str_time).strftime('%Y-%m-%d-%H-%M')
     
