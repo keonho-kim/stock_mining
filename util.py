f5426e3922b22a85a06f3b9f293128f870230be7
keonho-kim <keonhok0315@gmail.com>
2021-02-28 00:23:54 -0500

    commit

--- util.py
+++ util.py
@@ -210,10 +210,10 @@ def extract_word(word, result_type):
 
     if type(word) != float and word != '[removed]':
         word = word.lower()
+        word = re.sub('\n', ' ', word)
         word = re.sub('\'', ' ', word)
         word = re.sub('&amp;', '&', word)
-        word = re.sub('\n', ' ', word, 0, re.I|re.S)
-        word = word_tokenize(word)
+        word = word.split(' ')
         word_filtered= [w for w in word if not w in stop_words]
         word_filtered = [w for w in word_filtered if nonPunct.match(w)]
     
